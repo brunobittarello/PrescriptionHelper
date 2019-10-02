5daea907d77ae1e4794c65073fa1bf30efbec77c
Bruno Bittarello <brunobitta@hotmail.com>
2019-10-01 21:09:55 -0300

    Toolbar for PatientActivity

--- app/src/main/java/com/example/prescriptionhelper/PatientActivity.kt
+++ app/src/main/java/com/example/prescriptionhelper/PatientActivity.kt
@@ -1,22 +1,40 @@
 package com.example.prescriptionhelper
 
+
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_patient.*
 
+
+//https://developer.android.com/training/appbar?hl=pt-br
 class PatientActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.home -> {
+            onBackPressed()
+            true
+        }
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
 }
